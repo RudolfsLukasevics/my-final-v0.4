0813cd9a889fe9024f126dbf7976740cade48f7f
Rudy <rlukasevics25@coa.edu>
2024-11-22 10:56:27 -0500

    Add alt text to img in hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -21,7 +21,7 @@ const Hero = () => {
           </div>
         </article>
         <article className='hidden md:block'>
-          <img src={heroImg} className='h-80 lg:h-96' />
+          <img src={heroImg} className='h-80 lg:h-96' alt='image of a person looking over pie chart' />
         </article>
       </div>
     </div>
